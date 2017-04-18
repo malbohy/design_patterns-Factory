2e5da8d1e004d8ac0501713ad11ec376f2fab39f
mohamed elbohy <malbohy@gmail.com>
2017-04-18 17:04:52 +0200

    adjust main file

--- Main.go
+++ Main.go
@@ -1,19 +1,39 @@
 package main
+
 import (
+	"fmt"
+
 	"./Factory"
 	"./Factory/Notification"
-	"fmt"
 )
 
-func main(){
-	fmt.Println("moahemd")
-	message , _ := Factory.GetNotifier(1)
-	fmt.Println(message)
-	//
-	notification := Notification.Notification{"mohamed", "says heelo from main"}
-	messagerecived :=  message.SendNotification(notification)
-	//
-	fmt.Println(messagerecived)
+func main() {
+	sendAnSMSNotification()
+	sendAnEmailNotification()
+}
 
+func sendAnSMSNotification() {
+	SMSmessage, errorResult := Factory.GetNotifier(Notification.SMS)
+	mobileNumber := "+201114283495"
+	SMSContent := "This Is An Sms Content"
+	if errorResult == nil {
+		SMSnotification := Notification.Notification{mobileNumber, SMSContent}
+		messagerecived := SMSmessage.SendNotification(SMSnotification)
+		fmt.Println(messagerecived)
+	} else {
+		fmt.Println("there is an error happened", errorResult)
+	}
+}
 
+func sendAnEmailNotification() {
+	emailmessage, errorResult := Factory.GetNotifier(Notification.Email)
+	email := "malbohy@gmail.com"
+	emailContent := "This Is An Email Content"
+	if errorResult == nil {
+		emailNotification := Notification.Notification{email, emailContent}
+		messagerecived := emailmessage.SendNotification(emailNotification)
+		fmt.Println(messagerecived)
+	} else {
+		fmt.Println("there is an error happened", errorResult)
+	}
 }
